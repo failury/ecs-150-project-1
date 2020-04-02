63763ab684453dd41d23368d55a75ebe517b4788
failury <2760085283@qq.com>
2020-04-02 12:19:13 -0700

    update

---
+++ cmake-build-debug/CMakeFiles/ecs_150_project_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ecs_150_project_1.dir/main.cpp.o"
+  "ecs_150_project_1.pdb"
+  "ecs_150_project_1.exe"
+  "libecs_150_project_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ecs_150_project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ecs_150_project_1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ecs_150_project_1.
+# This may be replaced when dependencies are built.
